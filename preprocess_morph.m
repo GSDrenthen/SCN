a0fb6a1f772126b18a1a1aaacb9c41bef6f9bde9
Gerhard Drenthen <40467047+GSDrenthen@users.noreply.github.com>
2021-06-16 14:43:31 +0200

    Update preprocess_morph.m

--- preprocess_morph.m
+++ preprocess_morph.m
@@ -1,6 +1,6 @@
-# Preprocessing morphological measure (e.g. cortical thickness)
-# X_lin : matrix with variables to be regressed out (e.g. age and sex)
-# y_lin : vector with morphology measure
+% Preprocessing morphological measure (e.g. cortical thickness)
+% X_lin : matrix with variables to be regressed out (e.g. age and sex)
+% y_lin : vector with morphology measure
 function [corrected_morph] = preprocess_morph(X_lin,y_lin)
 for n = 1:size(y_lin,2)
     linearmodel = fitlm(X_lin,y_lin(1:end,n));
